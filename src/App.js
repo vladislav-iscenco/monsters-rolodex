eb77e74bdc490a8b938c6fe0a9e3941a40cbd111
Vladislav Iscenco <vladislav.iscenco@gmail.com>
2021-10-12 12:47:14 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -25,11 +25,12 @@ class App extends Component {
 
   render() {
     const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
+    const filteredMonsters = monsters.filter(monster =>  
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={this.handleChange}
